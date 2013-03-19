b48802b30a7dd60ce1f5834c8501870da210849c
Igor Zubkov <icesik@altlinux.org>
2013-03-19 07:06:42 +0200

    Create template-context-processor that adds django.conf.settings to the context

--- infodbproject/infodb/views.py
+++ infodbproject/infodb/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
+
 from infodb.models import Person
 
 
 def main(request):
     persons = Person.objects.all()
-    return render_to_response('main/index.html', {'persons': persons})
+    return render_to_response('main/index.html', {'persons': persons},
+        context_instance=RequestContext(request))
